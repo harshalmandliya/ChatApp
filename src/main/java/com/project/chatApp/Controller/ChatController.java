22a8b5e296e70065a82ea3cc8774bc8913b4928c
harshalmandliya <harshalmandliya3@gmail.com>
2025-11-28 16:09:33 +0530

    complete chat app added

---
+++ src/main/java/com/project/chatApp/Controller/ChatController.java
@@ -0,0 +1,21 @@
+package com.project.chatApp.Controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.project.chatApp.model.ChatMessage;
+
+@Controller
+public class ChatController {
+	@MessageMapping("/sendMessage")
+	@SendTo("/topic/messages")
+public ChatMessage sendMessage(ChatMessage message) {
+	return message;
+}
+	@GetMapping("chat")
+	public String chat() {
+		return "chat";
+	}
+}
---
+++ src/main/java/com/project/chatApp/model/ChatMessage.java
@@ -0,0 +1,30 @@
+package com.project.chatApp.model;
+
+public class ChatMessage {
+	
+private Long id;
+private String sender;
+private String content;
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getSender() {
+	return sender;
+}
+public void setSender(String sender) {
+	this.sender = sender;
+}
+public String getContent() {
+	return content;
+}
+public void setContent(String content) {
+	this.content = content;
+}
+@Override
+public String toString() {
+	return "ChatMessage [id=" + id + ", sender=" + sender + ", content=" + content + "]";
+}
+}
